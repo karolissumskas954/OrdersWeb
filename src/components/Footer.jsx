efd4512984970cdee2d0e1679e942086d64873fa
Karolis <karolissumskas954@gmail.com>
2023-06-29 15:36:38 +0300

    [ADD] Button functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,11 @@ import { Link } from 'react-router-dom'
 import { footerLT } from '../constants'
 
 const Footer = () => {
+  const openInNewTab = url => {
+    window.open(url, '_blank', 'noopener,noreferrer');
+  };
+
+
   return (
     <>
       <div className='bg-black h-[100%] rounded-[20px] max-sm:hidden mt-[-35px] flex flex-row justify-center'>
@@ -18,24 +23,24 @@ const Footer = () => {
           <h1 className='p-4 md:hidden font-poppins text-white text-[20px]'>{footerLT[2].title}</h1>
         </div>
         <div className='flex flex-row justify-end w-1/4 mr-4'>
-          <img src={facebook} className='md:flex hidden w-full max-w-[40px] mx-2' />
-          <img src={twitter} className='md:flex hidden w-full max-w-[40px] mx-2' />
-          <img src={instagram} className='md:flex hidden w-full max-w-[40px] mx-2' />
-          <img src={messenger} className='md:flex hidden w-full max-w-[40px] mx-2' />
-          <img src={facebook} className='md:hidden w-full max-w-[30px] mx-1' />
-          <img src={twitter} className='md:hidden w-full max-w-[30px] mx-1' />
-          <img src={instagram} className='md:hidden w-full max-w-[30px] mx-1' />
-          <img src={messenger} className='md:hidden w-full max-w-[30px] mx-1' />
+          <img src={facebook} onClick={()=> openInNewTab('https://www.facebook.com/')} className='md:flex hidden w-full max-w-[40px] mx-2 cursor-pointer' />
+          <img src={twitter} onClick={()=> openInNewTab('https://twitter.com/')} className='md:flex hidden w-full max-w-[40px] mx-2 cursor-pointer' />
+          <img src={instagram} onClick={()=> openInNewTab('https://www.instagram.com/')} className='md:flex hidden w-full max-w-[40px] mx-2 cursor-pointer' />
+          <img src={messenger} onClick={()=> openInNewTab('https://www.messenger.com/')} className='md:flex hidden w-full max-w-[40px] mx-2 cursor-pointer' />
+          <img src={facebook} onClick={()=> openInNewTab('https://www.facebook.com/')} className='md:hidden w-full max-w-[30px] mx-1 cursor-pointer' />
+          <img src={twitter} onClick={()=> openInNewTab('https://twitter.com/')} className='md:hidden w-full max-w-[30px] mx-1 cursor-pointer' />
+          <img src={instagram} onClick={()=> openInNewTab('https://www.instagram.com/')} className='md:hidden w-full max-w-[30px] mx-1 cursor-pointer' />
+          <img src={messenger} onClick={()=> openInNewTab('https://www.messenger.com/')} className='md:hidden w-full max-w-[30px] mx-1 cursor-pointer' />
         </div>
       </div>
       <div className='bg-black h-[100%] rounded-[20px] sm:hidden mt-[-35px] flex justify-center items-center'>
         <div className="flex justify-center items-center mb-1 w-full">
           <div className='flex flex-row justify-center mt-1'>
           <h1 className='p-2 font-poppins text-white text-[16px] '>{footerLT[2].title}</h1>
-          <img src={facebook} className='w-full max-w-[28px] min-w-[15px] mx-2' />
-          <img src={twitter} className='w-full max-w-[28px] min-w-[15px] mx-2' />
-          <img src={instagram} className='w-full max-w-[28px] min-w-[15px] mx-2' />
-          <img src={messenger} className='w-full max-w-[28px] min-w-[15px] mx-2' />
+          <img src={facebook} onClick={()=> openInNewTab('https://www.facebook.com/')} className='w-full max-w-[28px] min-w-[15px] mx-2' />
+          <img src={twitter} onClick={()=> openInNewTab('https://twitter.com/')} className='w-full max-w-[28px] min-w-[15px] mx-2' />
+          <img src={instagram} onClick={()=> openInNewTab('https://www.instagram.com/')} className='w-full max-w-[28px] min-w-[15px] mx-2' />
+          <img src={messenger} onClick={()=> openInNewTab('https://www.messenger.com/')} className='w-full max-w-[28px] min-w-[15px] mx-2' />
           </div>
         </div>
       </div>
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import { questionLT } from '../constants'
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  Typography,
+} from "@material-tailwind/react";
 
 const Questions = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const handleName = event => {
+    setName(event.target.value);
+  };
+  const handleEmail = event => {
+    setEmail(event.target.value);
+  };
+  const handleText = event => {
+    setText(event.target.value);
+  };
+  const handleOpen = () => {
+    setOpen(!open);
+    setName("");
+    setEmail("");
+    setText("");
+  }
   return (
     <>
       <div className="flex justify-center items-center bg-green-gradient rounded-[20px] h-[200%] max-md:hidden">
@@ -13,14 +40,14 @@ const Questions = () => {
           <div className="flex justify-center items-center">
             <div className="w-1/2 text-right mx-1">
 
-              <input type="text" placeholder="Vardas Pavardė" className="border border-gray-300 p-2 rounded-md w-[70%]" />
+              <input type="text" placeholder="Vardas Pavardė" onChange={handleName} value={name} className="border border-gray-300 p-2 rounded-md w-[70%]" />
             </div>
             <div className="w-1/2 text-left mx-1">
-              <input type="text" placeholder="El. Paštas" className="border border-gray-300 p-2 rounded-md w-[70%]" />
+              <input type="text" placeholder="El. Paštas" onChange={handleEmail} value={email} className="border border-gray-300 p-2 rounded-md w-[70%]" />
             </div>
           </div>
-          <textarea className="my-5 mx-[15%] block w-[70%] px-4 py-2 leading-tight border border-gray-300 rounded appearance-none focus:outline-blue focus:bg-white " rows="3" placeholder="Tekstas"></textarea>
-          <p className='p-4 font-poppins font-regular underline text-white text-[20px] text-left mx-[13%] mt-[-25px]'>Siųsti</p>
+          <textarea onChange={handleText} value={text} className="my-5 mx-[15%] block w-[70%] px-4 py-2 leading-tight border border-gray-300 rounded appearance-none focus:outline-blue focus:bg-white " rows="3" placeholder="Tekstas"></textarea>
+          <p onClick={handleOpen} className='p-4 font-poppins cursor-pointer font-regular underline text-white text-[20px] text-left mx-[13%] mt-[-25px]'>Siųsti</p>
         </div>
       </div>
 
@@ -33,25 +60,48 @@ const Questions = () => {
           <div className="flex justify-center items-center">
             <div className="w-1/2 text-right mx-1">
 
-              <input type="text" placeholder="Vardas Pavardė" className="border border-gray-300 p-2 rounded-md w-[70%]" />
+              <input type="text" placeholder="Vardas Pavardė" onChange={handleName} value={name} className="border border-gray-300 p-2 rounded-md w-[70%]" />
             </div>
             <div className="w-1/2 text-left mx-1">
-              <input type="text" placeholder="El. Paštas" className="border border-gray-300 p-2 rounded-md w-[70%]" />
+              <input type="text" placeholder="El. Paštas" onChange={handleEmail} value={email} className="border border-gray-300 p-2 rounded-md w-[70%]" />
             </div>
           </div>
-          <textarea className="my-5 mx-[15%] block w-[70%] px-4 py-2 leading-tight border border-gray-300 rounded appearance-none focus:outline-blue focus:bg-white " rows="3" placeholder="Tekstas"></textarea>
-          <p className='p-4 font-poppins font-regular underline text-white text-[16px] text-left mx-[12%] mt-[-25px]'>Siųsti</p>
+          <textarea onChange={handleText} value={text} className="my-5 mx-[15%] block w-[70%] px-4 py-2 leading-tight border border-gray-300 rounded appearance-none focus:outline-blue focus:bg-white " rows="3" placeholder="Tekstas"></textarea>
+          <p onClick={handleOpen} className='p-4 font-poppins cursor-pointer font-regular underline text-white text-[16px] text-left mx-[12%] mt-[-25px]'>Siųsti</p>
         </div>
       </div>
       <div className="flex flex-col justify-center items-center bg-green-gradient rounded-[28px] h-[200%] sm:hidden ">
         <h1 className='p-4 font-poppins font-bold text-white text-[30px]'>{questionLT[0].title}</h1>
         <p className='p-4 font-poppins font-regular text-white text-[18px] mt-[-30px]'>{questionLT[1].title}</p>
         <div className='flex flex-col items-center mx-3 w-[85%]'>
-          <input type="text" placeholder="Vardas Pavardė" className="my-2 border border-gray-300 p-2 rounded-md w-[95%]" />
-          <input type="text" placeholder="El. Paštas" className="border border-gray-300 p-2 rounded-md w-[95%]" />
-          <textarea className="my-2 block w-[95%]  px-2 py-2 leading-tight border border-gray-300 rounded appearance-none focus:outline-blue focus:bg-white " rows="3" placeholder="Tekstas"></textarea>
-          <p className='p-4 font-poppins font-regular underline text-white text-[16px] mx-[12%] mt-[-16px] mb-8'>Siųsti</p>
+          <input type="text" placeholder="Vardas Pavardė" onChange={handleName} value={name} className="my-2 border border-gray-300 p-2 rounded-md w-[95%]" />
+          <input type="text" placeholder="El. Paštas" onChange={handleEmail} value={email} className="border border-gray-300 p-2 rounded-md w-[95%]" />
+          <textarea onChange={handleText} value={text} className="my-2 block w-[95%]  px-2 py-2 leading-tight border border-gray-300 rounded appearance-none focus:outline-blue focus:bg-white " rows="3" placeholder="Tekstas"></textarea>
+          <p onClick={handleOpen} className='p-4 font-poppins cursor-pointer font-regular underline text-white text-[16px] mx-[12%] mt-[-16px] mb-8'>Siųsti</p>
         </div>
+      </div>
+
+      <div>
+        <Fragment >
+          <Dialog open={open} handler={handleOpen} className='w-[90%] sm:w-[40%]'>
+            <DialogBody divider className="grid place-items-center gap-4">
+              <Typography color="red" variant="h4" className="font-poppins">
+                Jūsų žinutė sėkmingai išsiųsta!
+              </Typography>
+              <Typography className="text-center font-poppins">
+                Netrukus su jumis susisieksime.
+              </Typography>
+            </DialogBody>
+            <DialogFooter className="space-x-2 items-center">
+            <Button variant="text" color="grey" onClick={handleOpen} className='font-poppins outline-red'>
+                Išeiti
+              </Button>
+              <Button variant="text" color="white" onClick={handleOpen} className='font-poppins bg-green outline-none'>
+                TĘSTI
+              </Button>
+            </DialogFooter>
+          </Dialog>
+        </Fragment>
       </div>
     </>
   )
